2d8ed5c20a1b53d6340b63c382f408c20b3ec2e7
mauricio <jourdanmauricio@gmail.com>
2023-03-04 15:41:33 -0300

    .

--- src/pages/Sections/About/About.jsx
+++ src/pages/Sections/About/About.jsx
@@ -1,11 +1,19 @@
 import ReactQuill from 'react-quill';
 import { useEffect, useState } from 'react';
 import { getSections } from '@/services/api/sections';
-import { quillSimpleModules } from '@/config/constants';
-import { updateSubsection } from '../../../services/api/sections';
+// import { quillSimpleModules } from '@/config/constants';
+import { updateSubsection } from '@/services/api/sections';
+import useEditor from '@/config/useEditor';
+import SectionType from '../SectionType/SectionType';
 
 const About = () => {
   const [section, setSection] = useState();
+
+  const imageHandler = async () => {
+    // openModal();
+  };
+
+  const { modules } = useEditor({ imageHandler });
 
   useEffect(() => {
     fetchData();
@@ -13,13 +21,13 @@ const About = () => {
 
   const fetchData = async () => {
     const data = await getSections();
-    const about = data.find((el) => el.name === 'Sobre mi');
+    const about = data.find((el) => el.name === 'about');
     setSection(about);
   };
 
   console.log('SECTION', section);
 
-  const handleChange = (name, value) => {
+  const onChange = (name, value) => {
     setSection({
       ...section,
       [name]: value,
@@ -58,19 +66,11 @@ const About = () => {
     <>
       {section && (
         <>
-          <div className="form__group w-full">
-            <label className="form__label">Posición Título</label>
-            <select
-              name="type"
-              onChange={(e) => handleChange(e.target.name, e.target.value)}
-              className="form__input border-gray-500"
-              value={section.type}
-            >
-              <option value="TEXT">Texto</option>
-              <option value="CARD">Tarjeta</option>
-              <option value="SPLIT">Pantalla divida</option>
-            </select>
-          </div>
+          <SectionType
+            section={section}
+            onChange={onChange}
+            handleChangeSubsection={handleChangeSubsection}
+          />
 
           <div className="form__group w-full editor">
             <label className="form__label">Contenido</label>
@@ -82,8 +82,7 @@ const About = () => {
                 handleChangeSubsection('content', e, section.subsections[0].id)
               }
               placeholder={'Write something awesome...'}
-              modules={quillSimpleModules}
-              // formats={section.type === 'TEXT' ? formats : ''}
+              modules={modules}
             />
           </div>
           <div className="actions">
---
+++ src/pages/Sections/SectionType/SectionType.jsx
@@ -0,0 +1,213 @@
+import AddPicture from '../AddPicture/AddPicture';
+
+const SectionType = ({ section, onChange, handleChangeSubsection }) => {
+  return (
+    <>
+      <div className="p-2 sm:p-10">
+        <div className="flex flex-col sm:flex-row justify-between">
+          <label>
+            <input
+              type="radio"
+              id=""
+              name="type"
+              value="TEXT"
+              checked={section.type === 'TEXT'}
+              onChange={(e) => onChange(e.target.name, e.target.value)}
+            />
+            <span className="ml-2">Texto</span>
+          </label>
+          <label>
+            <input
+              type="radio"
+              id=""
+              name="type"
+              value="CARD"
+              checked={section.type === 'CARD'}
+              onChange={(e) => onChange(e.target.name, e.target.value)}
+            />
+            <span className="ml-2">Tarjeta</span>
+          </label>
+          <label>
+            <input
+              type="radio"
+              id=""
+              name="type"
+              value="SPLIT"
+              checked={section.type === 'SPLIT'}
+              onChange={(e) => onChange(e.target.name, e.target.value)}
+            />
+            <span className="ml-2">Pantilla dividida</span>
+          </label>
+        </div>
+
+        <div className="mt-10">
+          {section.type === 'TEXT' && (
+            <div className="flex flex-col justify-between gap-2 w-full p-2 h-[300px] rounded border-4 border-solid border-teal-700">
+              <p className="text-sm text-center">
+                <span>Título</span>
+              </p>
+              <p className="text-xs">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+                earum cum illum saepe et consequuntur, ratione deleniti enim
+                praesentium ipsum impedit esse eaque natus molestiae. Dolorem
+                voluptas architecto ex culpa.
+              </p>
+              <svg className="w-full md:w-1/2 mx-auto h-[50%]">
+                <rect
+                  width="100%"
+                  height="100%"
+                  stroke="gray"
+                  strokeWidth="2"
+                  fill="transparent"
+                ></rect>
+                <line
+                  x1="0"
+                  y1="0"
+                  x2="100%"
+                  y2="100%"
+                  stroke="gray"
+                  strokeWidth="2"
+                />
+                <line
+                  x1="100%"
+                  y1="0"
+                  x2="0"
+                  y2="100%"
+                  stroke="gray"
+                  strokeWidth="2"
+                />
+                Su navegador no soporta SVG :/
+              </svg>
+            </div>
+          )}
+
+          {section.type === 'CARD' && (
+            <div className="w-full p-2 h-[300px] rounded border-4 border-solid border-teal-700">
+              <div className="w-[300px] h-full m-auto overflow-hidden rounded border border-solid border-gray-700">
+                <svg className="w-full h-[40%]">
+                  <line
+                    x1="0"
+                    y1="0"
+                    x2="100%"
+                    y2="100%"
+                    stroke="gray"
+                    strokeWidth="2"
+                  />
+                  <line
+                    x1="100%"
+                    y1="0"
+                    x2="0"
+                    y2="100%"
+                    stroke="gray"
+                    strokeWidth="2"
+                  />
+                  <line
+                    x1="0"
+                    y1="100%"
+                    x2="100%"
+                    y2="100%"
+                    stroke="gray"
+                    strokeWidth="2"
+                  />
+                  Su navegador no soporta SVG :/
+                </svg>
+                <p className="text-sm text-center mt-2">
+                  <span>Título</span>
+                </p>
+                <p className="text-xs p-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+                  earum cum illum saepe et consequuntur, ratione deleniti enim
+                  praesentium ipsum impedit esse eaque natus molestiae. Dolorem
+                  voluptas architecto ex culpa.
+                </p>
+              </div>
+            </div>
+          )}
+
+          {section.type === 'SPLIT' && (
+            <div className="flex flex-col sm:flex-row justify-between gap-2 w-full p-2 h-[300px] rounded border-4 border-solid border-teal-700">
+              <div className="w-full sm:w-1/2 h-full overflow-hidden rounded border border-solid border-gray-700">
+                <svg className="w-full h-[100%]">
+                  <line
+                    x1="0"
+                    y1="0"
+                    x2="100%"
+                    y2="100%"
+                    stroke="gray"
+                    strokeWidth="2"
+                  />
+                  <line
+                    x1="100%"
+                    y1="0"
+                    x2="0"
+                    y2="100%"
+                    stroke="gray"
+                    strokeWidth="2"
+                  />
+                  <line
+                    x1="0"
+                    y1="100%"
+                    x2="100%"
+                    y2="100%"
+                    stroke="gray"
+                    strokeWidth="2"
+                  />
+                  Su navegador no soporta SVG :/
+                </svg>
+              </div>
+              <div className="w-full sm:w-1/2 overflow-hidden">
+                <p className="text-sm text-center mt-2">
+                  <span>Título</span>
+                </p>
+                <p className="text-xs p-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+                  earum cum illum saepe et consequuntur, ratione deleniti enim
+                  praesentium ipsum impedit esse eaque natus molestiae. Dolorem
+                  voluptas architecto ex culpa.
+                </p>
+                <p className="text-xs p-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+                  earum cum illum saepe et consequuntur, ratione deleniti enim
+                  praesentium ipsum impedit esse eaque natus molestiae. Dolorem
+                  voluptas architecto ex culpa.
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
+      {section.type !== 'TEXT' && (
+        <>
+          <div className="form__group w-full">
+            <label className="form__label">Título</label>
+            <input
+              onChange={(e) => onChange(e.target.name, e.target.value)}
+              onBlur={(e) => onBlurTitle(e.target.value)}
+              className="form__input border-gray-500 w-full"
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Título de post"
+              value={section.title}
+            />
+            {/* <p
+              className={`input__error ${
+                error.title ? 'opacity-100' : 'opacity-0'
+              }`}
+            >
+              {error.title}
+            </p> */}
+          </div>
+
+          <AddPicture
+            subsection={section.subsections[0]}
+            handleChangeSubsection={handleChangeSubsection}
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default SectionType;
